b0287812fb06dff1a224b9f40298c5186bc7e9e2
Dharmik Puri Goswami <dharmikpuri2004@gmail.com>
2023-05-12 12:58:06 +0530

    )(

--- middleware/uservalidator.js
+++ middleware/uservalidator.js
@@ -9,8 +9,8 @@ async function userValidator(req,res,next){
 
     let data= await UserModel.find({$or: [{"name" : req.body.username}, {"email" : req.body.email}]})
     if(data.length>0){
-        res.send({
-            message:"User already exist"
+        res.status(400).send({
+            error:"User already exist"
         })
     }else{
         next()
